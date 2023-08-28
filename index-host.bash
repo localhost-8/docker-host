7dba2236b471ee6c66fb877a4ba477092eb09a27
staffuser <119116574+staffuser@users.noreply.github.com>
2023-08-28 10:22:47 +0300

    Update index-host.bash

--- index-host.bash
+++ index-host.bash
@@ -1,3 +1,4 @@
+docker run -dp 80:80 docker/getting-started
 #Run docker
 npm run run-start --servername=https://staffuser.github.io/chat.com --upstreamurl=http://localhost:3000 --enblessl=yes --emailaddress=my@mail.ru 
 #Build docker
